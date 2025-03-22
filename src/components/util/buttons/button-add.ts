1cb01738edbd15c2d078d965637318b46d7e2419
Irasov <srnan@yandex.ru>
2025-03-22 18:12:36 +0500

    feat: add functionality for adding and deleting options

--- src/components/util/buttons/button-add.ts
+++ src/components/util/buttons/button-add.ts
@@ -1,4 +1,6 @@
 import Component from '../component';
+import Option from '../option/option';
+import type OptionsView from '../../view/main/options/options';
 import type { typeHTMLElement } from '../component';
 import { Events } from '../events-const';
 import './button.scss';
@@ -10,12 +12,17 @@ const btn: typeHTMLElement = {
 };
 
 export default class AddButton extends Component {
-  constructor() {
+  constructor(optionsView: OptionsView) {
     super(btn);
-    this.configureBtn();
+    this.configureBtn(optionsView);
   }
 
-  private configureBtn(): void {
-    this.addListner(Events.CLICK, () => {});
+  private configureBtn(optionsView: OptionsView): void {
+    this.addListner(Events.CLICK, () => {
+      optionsView.setIdOptions();
+      const option = new Option(optionsView.getLastIdOptions(), optionsView);
+      optionsView.setOption(optionsView.getLastIdOptions(), option);
+      optionsView.addOption(option);
+    });
   }
 }
--- src/components/view/main/options/options.ts
+++ src/components/view/main/options/options.ts
@@ -9,6 +9,7 @@ import ClearButton from '../../../util/buttons/button-clear';
 import SaveButton from '../../../util/buttons/button-save';
 import LoadButton from '../../../util/buttons/button-load';
 import type Option from '../../../util/option/option';
+import Component from '../../../util/component';
 
 const options: typeHTMLElement = {
   tag: 'div',
@@ -16,20 +17,50 @@ const options: typeHTMLElement = {
   classes: ['options'],
 };
 
+const optionsDiv: typeHTMLElement = {
+  tag: 'div',
+  content: '',
+  classes: ['options__block'],
+};
+
 export default class OptionsView extends View {
+  private optionsBlock: Component;
   private options: Map<number, Option> = new Map();
+  private idOptions: number[] = [];
   constructor(router: Router) {
     super(options);
+    this.optionsBlock = new Component(optionsDiv);
     this.configure(router);
+  }
+
+  public setOption(id: number, option: Option): void {
+    this.options.set(id, option);
   }
 
   public removeOption(key: number): void {
     this.options.delete(key);
   }
 
+  public setIdOptions(): void {
+    if (this.idOptions.length > 0) {
+      this.idOptions.push(this.idOptions[this.idOptions.length - 1] + 1);
+    } else {
+      this.idOptions.push(1);
+    }
+  }
+
+  public getLastIdOptions(): number {
+    return this.idOptions[this.idOptions.length - 1];
+  }
+
+  public addOption(option: Option): void {
+    this.optionsBlock.append(option);
+  }
+
   private configure(router: Router): void {
     this.getComponent().appendChildren([
-      new AddButton(),
+      this.optionsBlock,
+      new AddButton(this),
       new PasteButton(),
       new ClearButton(),
       new SaveButton(),
