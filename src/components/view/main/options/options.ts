07f2bf73974007d273e7cc670d2d2831bd0c073c
Irasov <srnan@yandex.ru>
2025-03-22 13:44:12 +0500

    feat: add buttons in options

--- src/components/view/main/options/options.ts
+++ src/components/view/main/options/options.ts
@@ -3,6 +3,11 @@ import type { typeHTMLElement } from '../../../util/component';
 import type Router from '../../../router/router';
 import StartButton from '../../../util/buttons/button-start';
 import './options.scss';
+import AddButton from '../../../util/buttons/button-add';
+import PasteButton from '../../../util/buttons/button-paste';
+import ClearButton from '../../../util/buttons/button-clear';
+import SaveButton from '../../../util/buttons/button-save';
+import LoadButton from '../../../util/buttons/button-load';
 
 const options: typeHTMLElement = {
   tag: 'div',
@@ -17,6 +22,6 @@ export default class OptionsView extends View {
   }
 
   private configure(router: Router): void {
-    this.getComponent().appendChildren([new StartButton(router)]);
+    this.getComponent().appendChildren([new AddButton(), new PasteButton(), new ClearButton(), new SaveButton(), new LoadButton(), new StartButton(router)]);
   }
 }
