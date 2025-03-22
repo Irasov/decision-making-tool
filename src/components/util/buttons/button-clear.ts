362215a930ff48eb9ca0c217f3ba83df488fe60f
Irasov <srnan@yandex.ru>
2025-03-22 13:37:45 +0500

    feat: add button-clear class

---
+++ src/components/util/buttons/button-clear.ts
@@ -0,0 +1,21 @@
+import Component from '../component';
+import type { typeHTMLElement } from '../component';
+import { Events } from '../events-const';
+import './button.scss';
+
+const btn: typeHTMLElement = {
+  tag: 'button',
+  content: '',
+  classes: ['decision__clear', 'btn'],
+};
+
+export default class ClearButton extends Component {
+  constructor() {
+    super(btn);
+    this.configureBtn();
+  }
+
+  private configureBtn(): void {
+    this.addListner(Events.CLICK, () => {});
+  }
+}
